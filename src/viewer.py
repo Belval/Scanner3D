cf7f0c7a3baa21df8a733ca3d09bc286344d1783
Edouard Belval <edouard@belval.org>
2020-09-13 21:18:40 -0400

    Add actual viewer

---
+++ src/viewer.py
@@ -0,0 +1,88 @@
+import argparse
+import math
+import time
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+import open3d as o3d
+
+def main(args):
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+    # Start streaming
+    pipeline.start(config)
+
+    # Get stream profile and camera intrinsics
+    profile = pipeline.get_active_profile()
+    depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
+    intrinsics = depth_profile.get_intrinsics()
+
+    # Processing blocks
+    pc = rs.pointcloud()
+    colorizer = rs.colorizer()
+
+    # Wait for a coherent pair of frames: depth and color
+    frames = pipeline.wait_for_frames()
+
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    print(color_image)
+
+    rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        o3d.geometry.Image(color_image),
+        o3d.geometry.Image(depth_image),
+        convert_rgb_to_intensity=False
+    )
+    pinhole_camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy)
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, pinhole_camera_intrinsic)
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    o3d.visualization.draw_geometries([pcd])
+
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
+    if args.mesh_reconstruction_method == "BPA":
+        distances = pcd.compute_nearest_neighbor_distance()
+        avg_dist = np.mean(distances)
+        radius = 3 * avg_dist
+        mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector([radius, radius * 2]))
+        if args.max_triangle_count > 0:
+            mesh = mesh.simplify_quadric_decimation(args.max_triangle_count)
+        if args.ensure_consistency:
+            mesh.remove_degenerate_triangles()
+            mesh.remove_duplicated_triangles()
+            mesh.remove_duplicated_vertices()
+            mesh.remove_non_manifold_edges()
+    elif args.mesh_reconstruction_method == "Poisson":
+        raise NotImplementedError()
+    else: 
+        raise NotImplementedError()
+
+    o3d.visualization.draw_geometries([mesh])
+
+    o3d.io.write_triangle_mesh(args.output_file, mesh)
+
+    pipeline.stop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Scan a 3D object and save the mesh file")
+    parser.add_argument(
+        "--output-file", type=str, nargs="?", help="Name of the output file", default="result.obj"
+    )
+    parser.add_argument(
+        "--mesh-reconstruction-method", type=str, nargs="?", help="Method that will be used to create a mesh from the point cloud. One of [BPA|Poisson]", default="BPA"
+    )
+    parser.add_argument(
+        "--max-triangle-count", type=int, nargs="?", help="Maximum triangle count, default is -1 (None)", default=-1
+    )
+    parser.add_argument(
+        "--ensure-consistency", action="store_true", help="Whether to clean the resulting mesh or not"
+    )
+    main(parser.parse_args())
